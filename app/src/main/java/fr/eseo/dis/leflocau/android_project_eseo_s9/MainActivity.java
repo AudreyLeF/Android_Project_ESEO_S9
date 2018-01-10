67a70826613055ac7872f79a507aed27f040d21b
audrey <audreysophie@hotmail.fr>
2018-01-10 14:05:25 +0100

    Initial commit

---
+++ app/src/main/java/fr/eseo/dis/leflocau/android_project_eseo_s9/MainActivity.java
@@ -0,0 +1,13 @@
+package fr.eseo.dis.leflocau.android_project_eseo_s9;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
